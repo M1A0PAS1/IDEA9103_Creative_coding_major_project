17a5c0d634ef795561a60c0383984292892be60a
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-11-06 18:31:23 +1100

    save img

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 //We need a variable to hold our image
 let img;
+let container;
+let imageSaved = false;
 
 //Let's make an object to hold the draw properties of the image
 let imgDrwPrps = { aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0 };
@@ -16,13 +18,23 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imgDrwPrps.aspect = img.width / img.height;
   calculateImageDrawProps();
+  container = createGraphics(img.width, img.height);
 }
 
 function draw() {
   background(220);
-  pointImg();
+  if (!imageSaved) {
+    pointImg(container);
+    saveHiddenCanvasAsImage(container);
+    imageSaved = true;
+  }
+
   //image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
 
+}
+
+function saveHiddenCanvasAsImage(canvas) {
+  canvas.save('myImg.jpg');
 }
 
 function windowResized() {
@@ -58,22 +70,22 @@ function calculateImageDrawProps() {
   }
 }
 
-function pointImg() {
+function pointImg(canvas) {
   for (let col = 0; col <= img.width; col += 2) {
     for (let row = 0; row <= img.height; row += 2) {
-      let xPos = col;
-      let yPos = row;
+      let xPos = map(col, 0, img.width, 0, width);
+      let yPos = map(row, 0, img.height, 0, height);
       let c = img.get(xPos, yPos);
-      push();
-      translate(xPos, yPos);
-      rotate(radians(random(360)));
-      noFill();
-      stroke(color(c));
-      strokeWeight(random(5));
-      point(xPos, yPos);
-      strokeWeight(random(3));
-      curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50);
-      pop();
+      canvas.push();
+      canvas.translate(xPos, yPos);
+      canvas.rotate(radians(random(360)));
+      canvas.noFill();
+      canvas.stroke(color(c));
+      canvas.strokeWeight(random(5));
+      canvas.point(xPos, yPos);
+      canvas.strokeWeight(random(3));
+      canvas.curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50);
+      canvas.pop();
     }
   }
 }
