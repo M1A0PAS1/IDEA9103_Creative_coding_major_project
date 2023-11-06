797950443627b8f18e519ef7dbf0f59e97be40bb
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-11-06 19:14:11 +1100

    Apply group code

--- sketch.js
+++ sketch.js
@@ -1,91 +1,96 @@
-//We need a variable to hold our image
-let img;
-let container;
-let imageSaved = false;
-
-//Let's make an object to hold the draw properties of the image
-let imgDrwPrps = { aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0 };
-
-//A variable for the canvas aspect ratio
-let canvasAspectRatio = 0;
-
-//let's load the image from disk
-function preload() {
-  img = loadImage('/assets/Claude_Monet,_Saint-Georges_majeur_au_crépuscule.jpg');
-}
+let color1, color2, color3;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  imgDrwPrps.aspect = img.width / img.height;
-  calculateImageDrawProps();
-  container = createGraphics(img.width, img.height);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+
+  color1 = color(32, 55, 69); 
+  color2 = color(102, 43, 59); 
+  color3 = color(50,30,40);
 }
 
 function draw() {
-  background(220);
-  if (!imageSaved) {
-    pointImg(container); 
-    saveImage(container); 
-    imageSaved = true;
-  }
-  
-  //image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
+  background(30);
+  rotateX(85);
 
+  translate(0, 0, -40);
+  for (var i = 0; i < 100; i++) {
+    beginShape();
+    for (var j = 0; j < 360; j += 10) {
+      var rad = i * 5;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var d = dist(0, 0, x, y);
+      var interpColor;
+      if (d <= 200) {
+        interpColor = lerpColor(color(196, 99, 85), color(189,120,51), map(d, 0, 200, 0, 1));
+      } else {
+        interpColor = lerpColor(color(209,134,61), color(88,142,189), map(d, 200.1, 500, 0, 1));
+      }
+      stroke(interpColor);
+      strokeWeight(2)
+      vertex(x, y);
+      noFill();
+    }
+    endShape(CLOSE);
+  }
+
+translate(0, 0, 100);
+rotateX(7);
+for (var i = 0; i < 100; i++) {
+  beginShape();
+  for (var j = 0; j < 360; j += 10) {
+    var rad = i * 5;
+    var x = rad * cos(j);
+    var y = rad * sin(j);
+    var d = dist(0, 0, x, y);
+    var interpColor;
+    if (d <= 200) {
+      interpColor = lerpColor(color(196, 99, 85), color(220, 147, 47), map(d, 0, 200, 0, 1));
+    } else {
+      interpColor = lerpColor(color(220, 147, 47), color(69,106,162), map(d, 200.1, 500, 0, 1));
+    }
+    stroke(interpColor);
+    strokeWeight(2)
+    vertex(x, y);
+    noFill();
+  }
+  endShape(CLOSE);
 }
 
-function saveImage(canvas) {
-  canvas.save('myImg.jpg');
+fill(120, 75, 50);
+
+beginShape();
+  for (let y = -130; y <= 75; y += 10) {
+    let lerpedColor;
+    if (y < -30) {
+      lerpedColor = lerpColor(color1, color2, map(y, -130, -30, 0, 1));
+    } else {
+      lerpedColor = lerpColor(color2, color3, map(y, -30, 75, 0, 1));
+    }
+    fill(lerpedColor);
+    vertex(-260, 75, -130);
+    vertex(-10, 75, -130);
+    vertex(-15, 75, -100);
+    vertex(-30, 75, -80);
+    vertex(-60, 75, -80);
+    vertex(-70, 75, -65);
+    vertex(-85, 75, -50);
+    vertex(-100, 75, -50);
+    vertex(-105, 75, -10);
+    vertex(-130, 75, 0);
+    vertex(-140, 75, -10);
+    vertex(-140, 75, 50);
+    vertex(-150, 75, 70);
+    vertex(-165, 75, 50);
+    vertex(-170, 75, -10);
+    vertex(-200, 75, -50);
+    vertex(-250, 75, -80);
+    noStroke();
+}
+endShape(CLOSE);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  calculateImageDrawProps();
-}
-
-function calculateImageDrawProps() {
-  //Calculate the aspect ratio of the canvas
-  canvasAspectRatio = width / height;
-  //if the image is wider than the canvas
-  if (imgDrwPrps.aspect > canvasAspectRatio) {
-    //then we will draw the image to the width of the canvas
-    imgDrwPrps.width = width;
-    //and calculate the height based on the aspect ratio
-    imgDrwPrps.height = width / imgDrwPrps.aspect;
-    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
-    imgDrwPrps.xOffset = 0;
-  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
-    //otherwise we will draw the image to the height of the canvas
-    imgDrwPrps.height = height;
-    //and calculate the width based on the aspect ratio
-    imgDrwPrps.width = height * imgDrwPrps.aspect;
-    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
-    imgDrwPrps.yOffset = 0;
-  }
-  else if (imgDrwPrps.aspect == canvasAspectRatio) {
-    //if the aspect ratios are the same then we can draw the image to the canvas size
-    imgDrwPrps.width = width;
-    imgDrwPrps.height = height;
-    imgDrwPrps.xOffset = 0;
-    imgDrwPrps.yOffset = 0;
-  }
-}
-
-function pointImg(canvas) {
-  for (let col = 0; col <= img.width; col += 2) {
-    for (let row = 0; row <= img.height; row += 2) {
-      let xPos = map(col, 0, img.width, 0, width);
-      let yPos = map(row, 0, img.height, 0, height);
-      let c = img.get(xPos, yPos);
-      canvas.push();
-      canvas.translate(xPos, yPos);
-      canvas.rotate(radians(random(360)));
-      canvas.noFill();
-      canvas.stroke(color(c));
-      canvas.strokeWeight(random(5));
-      canvas.point(xPos, yPos);
-      canvas.strokeWeight(random(3));
-      canvas.curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50);
-      canvas.pop();
-    }
-  }
+resizeCanvas(windowWidth, windowHeight);
 }
