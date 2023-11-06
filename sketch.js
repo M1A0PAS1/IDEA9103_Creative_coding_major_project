86d827e6c839192bfdffb8646ddc79470518ac38
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-11-06 16:47:29 +1100

    windowResized

--- sketch.js
+++ sketch.js
@@ -1,7 +1,49 @@
+//Let's make an object to hold the draw properties of the image
+let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
+
+//A variable for the canvas aspect ratio
+let canvasAspectRatio = 0;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  imgDrwPrps.aspect = 2480 / 1578;
+  calculateImageDrawProps();
 }
 
 function draw() {
   background(220);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  calculateImageDrawProps();
+}
+
+function calculateImageDrawProps() {
+  //Calculate the aspect ratio of the canvas
+  canvasAspectRatio = width / height;
+  //if the image is wider than the canvas
+  if (imgDrwPrps.aspect > canvasAspectRatio) {
+    //then we will draw the image to the width of the canvas
+    imgDrwPrps.width = width;
+    //and calculate the height based on the aspect ratio
+    imgDrwPrps.height = width / imgDrwPrps.aspect;
+    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
+    imgDrwPrps.xOffset = 0;
+  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
+    //otherwise we will draw the image to the height of the canvas
+    imgDrwPrps.height = height;
+    //and calculate the width based on the aspect ratio
+    imgDrwPrps.width = height * imgDrwPrps.aspect;
+    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
+    imgDrwPrps.yOffset = 0;
+  }
+  else if (imgDrwPrps.aspect == canvasAspectRatio) {
+    //if the aspect ratios are the same then we can draw the image to the canvas size
+    imgDrwPrps.width = width;
+    imgDrwPrps.height = height;
+    imgDrwPrps.xOffset = 0;
+    imgDrwPrps.yOffset = 0;
+  }
 }
